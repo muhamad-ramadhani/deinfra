4c1d5a2d0e617b60f463bc681ec812cd31382f26
Muhamad Ramadhani <72949170+muhamad-ramadhani@users.noreply.github.com>
2022-10-30 03:35:45 +0700

    Create definfra.sh

---
+++ definfra.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo -e "\e[92m"
+echo  "    ____                      _                        _    _         _                  ";
+echo  "   |  _ \ ___ _ __ ___  _   _| |_   _ _ __   __ _     / \  (_)_ __ __| |_ __ ___  _ __   ";
+echo  "   | |_) / _ \ '_ ` _ \| | | | | | | | '_ \ / _` |   / _ \ | | '__/ _` | '__/ _ \| '_ \  ";
+echo  "   |  __/  __/ | | | | | |_| | | |_| | | | | (_| |  / ___ \| | | | (_| | | | (_) | |_) | ";
+echo  "   |_|   \___|_| |_| |_|\__,_|_|\__,_|_| |_|\__, | /_/   \_\_|_|  \__,_|_|  \___/| .__/  ";
+echo  "                                            |___/                                |_|     ";
+echo -e "\e[0m"
+echo "===================================================================" 
+
+echo -e '\e[36mGarapan :\e[39m' Deinfra Incentivized Testnet
+echo -e '\e[36mAuthor :\e[39m' Rama | PemulungAirdropID
+echo -e '\e[36mTelegram Group :\e[39m' @DiskusiPemulungAirdrop
+echo -e '\e[36mTelegram Channel :\e[39m' @PemulungAirdropID
+echo "======================================="
+
