8a1cdbb55ebda99b59c9871a777b777284017cd1
Muhamad Ramadhani <72949170+muhamad-ramadhani@users.noreply.github.com>
2022-10-30 03:43:01 +0700

    Update deinfra.sh

--- deinfra.sh
+++ deinfra.sh
@@ -17,6 +17,8 @@ echo -e '\e[36mTelegram Group :\e[39m' @DiskusiPemulungAirdrop
 echo -e '\e[36mTelegram Channel :\e[39m' @PemulungAirdropID
 echo "======================================="
 
+sleep 2
+
 echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
 # update
 sudo apt update -y && sudo apt install jq && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin && sudo apt install docker-compose
